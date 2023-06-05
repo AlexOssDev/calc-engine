bf5b099f07c6302d2ec85377f90cf4a888775d46
alexdev <alexossdev@gmail.com>
2023-06-05 16:03:35 +0300

    fix: add page title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,10 @@
 	import { IconBrandGithub } from '@tabler/icons-svelte';
 </script>
 
+<svelte:head>
+	<title>calculator engine</title>
+</svelte:head>
+
 <div class="top-0 min-h-screen dark:bg-slate-700 dark:text-slate-200 md:flex md:justify-between">
 	<slot />
 </div>
@@ -12,5 +16,5 @@
 	class="fixed bottom-0 flex rounded-lg p-2 text-xl text-transparent outline-none transition-colors hover:bg-white hover:text-sky-400 hover:dark:bg-slate-700 hover:dark:text-sky-300"
 >
 	<IconBrandGithub />
-	v0.1.0-beta
+	v0.1.0
 </a>
