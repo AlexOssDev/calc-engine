658af7b3fe54050b1a70db9b6cb38357dafc4e9c
alexdev <alexossdev@gmail.com>
2023-06-05 15:56:23 +0300

    feat: relatively-good-looking StackList

--- src/lib/StackList.svelte
+++ src/lib/StackList.svelte
@@ -1,14 +1,39 @@
 <script lang="ts">
+	import { IconEye, IconEyeOff } from '@tabler/icons-svelte';
 	import { dataStore } from './storage';
+
+	$: currentCommonState = $dataStore.map((stack) => stack.hidden).some((hidden) => !hidden);
+
+	function toggleAll() {
+		dataStore.set($dataStore.map((stack) => ({ ...stack, hidden: currentCommonState })));
+	}
 </script>
 
-<ul>
+<ul class="p-3 pb-0">
+	<li>
+		<button class="flex w-full justify-between gap-4 p-3 text-left" on:click={toggleAll}>
+			{#if currentCommonState}
+				Hide all
+				<IconEye />
+			{:else}
+				Show all
+				<IconEyeOff />
+			{/if}
+		</button>
+	</li>
 	{#each $dataStore as stack}
-		<li class="m-3 flex justify-between gap-3 first:mt-0">
-			<button class="w-full text-left" on:click={() => (stack.hidden = !stack.hidden)}>
+		<li>
+			<button
+				class="flex w-full justify-between gap-4 p-3 text-left"
+				on:click={() => (stack.hidden = !stack.hidden)}
+			>
 				{stack.name}
+				{#if stack.hidden}
+					<IconEyeOff />
+				{:else}
+					<IconEye />
+				{/if}
 			</button>
-			{stack.hidden ? '✘' : '✔'}
 		</li>
 	{/each}
 </ul>
